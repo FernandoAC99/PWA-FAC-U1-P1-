b2bdc6e75ba3afb10677986b4ae73efe3b733d01
FernandoAC99 <fernandoarteagacortez@gmail.com>
2021-12-04 21:32:35 -0600

    Segundo commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+let url = window.location.href
+let swDirect = "/PWA-FAC-U1-P1/sw.js"
+let rickFlag = false
+
+if(navigator.serviceWorker){
+    console.log("Muy bien");
+    if(url.includes('localhost')){
+        swDirect = "/sw.js"
+    }
+    navigator.serviceWorker.register(swDirect)
+}else{
+    console.log("Nada bien")
+}
 console.log('hola desde el js');
 if(navigator.serviceWorker){
     console.log('Todo bien, adelante bro');
